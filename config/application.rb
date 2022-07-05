8d9666875bd80406cf656c01c5b648342ca18736
fumtas1k <fumta01a7@gmail.com>
2022-07-05 21:32:48 +0900

    fix:記述場所修正

--- config/application.rb
+++ config/application.rb
@@ -13,20 +13,15 @@ module AjaxSample
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-  end
-
-  config.generators do |g|
-    g.test_framework :rspec,
-    fixtures: true,
-    view_specs: false,
-    helper_specs: false,
-    routing_specs: false,
-    controller_specs: false,
-    request_specs: false
-    g.fixture_replacement :factory_bot, dir: "spec/factories"
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: false,
+      request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
